933df8a25bef8cc15d2f84559bf52070941944a9
MarcelineVQ <marcythevq@gmail.com>
2024-05-14 15:22:31 -0700

    [ change ] major overhaul of addon, plates store their own state
    [ change ] cc check uses spell id to get names

--- TankPlates.lua
+++ TankPlates.lua
@@ -14,175 +14,244 @@ if not SetAutoloot then
   return
 end
 
-local tracked_units = {}
+-- local plates = {}
 
 local cc_spells = {
-  ["Interface\\Icons\\Spell_Nature_Sleep"] = "Hibernate", -- handles magic dust too
-  ["Interface\\Icons\\Spell_Nature_Polymorph"] = "Polymorph",
-  ["Interface\\Icons\\Spell_Nature_Slow"] = "Shackle Undead",
-  ["Interface\\Icons\\Ability_Gouge"] = "Gouge",
-  ["Interface\\Icons\\Ability_Sap"] = "Sap",
-  ["Interface\\Icons\\Ability_Hunter_Pet_Turtle"] = "Polymorph: Turtle",
-  ["Interface\\Icons\\Spell_Magic_PolymorphPig"] = "Polymorph: Pig",
-  ["Interface\\Icons\\Spell_Nature_Polymorph_Cow"] = "Polymorph: Cow",
-  ["Interface\\Icons\\Spell_Magic_PolymorphChicken"] = "Polymorph: Chicken",
+  "Hibernate",
+  "Magic Dust",
+  "Polymorph",
+  "Shackle Undead",
+  "Gouge",
+  "Sap",
+  "Polymorph: Turtle",
+  "Polymorph: Pig",
+  "Polymorph: Cow",
+  "Polymorph: Chicken",
 }
 
-local function UnitIsCC(unit) -- shackle and sheep rly,hibernate,magic dust
+-- shackle, sheep, hibernate, magic dust, etc
+local function UnitIsCC(unit)
   for i=1,40 do
-    local dTexture,_ = UnitDebuff(unit,i)
-    if dTexture and cc_spells[dTexture] then
-      return true
+    local dTexture,_,_,spell_id = UnitDebuff(unit,i)
+    local name = SpellInfo(spell_id)
+    for _,spell in ipairs(cc_spells) do
+      if name == spell then
+        return true
+      end
     end
   end
   return false
 end
 
-local function UpdatePlate(unitGUID)
-  local _,playerGUID = UnitExists("player")
-  local _,targetGUID = UnitExists(unitGUID.."target")
-  -- is this plate even stored yet?
-  -- Shouldn't happen, but does
-  if not tracked_units[unitGUID] then return end
+-- local function UpdatePlate(vplate)
+--   if not plates[vplate.unit] then
+--     tp_print("|cffff0000impossible error:|r plate not tracked, report this")
+--     return
+--   end
+--   local _,playerGUID = UnitExists("player")
+--   local _,targetGUID = UnitExists(vplate.unit.."target")
+--   local unitGUID = vplate.unit
+--   local healthbar = vplate.healthbar
 
-  -- if we can attack and we're in combat, proceed
-  if UnitAffectingCombat("player") and (targetGUID or (UnitReaction(unitGUID,playerGUID) < 4)) then
-    -- cc'd mob, avoid it
-    if not targetGUID and tracked_units[unitGUID].cc == true then
-      debug_print("cc " .. unitGUID .. " " .. UnitName(unitGUID))
-      tracked_units[unitGUID].color = {r = 0, g = 0, b = 0, a = 0.5}
-    -- you have aggro
-    elseif targetGUID and (targetGUID == playerGUID) then
-      debug_print("aggro " .. unitGUID .. " " .. UnitName(unitGUID))
-      tracked_units[unitGUID].color = {r = 0, g = 1, b = 0, a = 1}
-      tracked_units[unitGUID].targeting = playerGUID
-    -- targeting someone else
-    elseif targetGUID and (targetGUID ~= playerGUID) then
-      debug_print("aggro not-you: " .. unitGUID .. " " .. UnitName(unitGUID))
-      tracked_units[unitGUID].color = {r = 1, g = 0, b = 0, a = 1}
-      tracked_units[unitGUID].targeting = targetGUID
-    else
-      debug_print("not aggro " .. unitGUID .. " " .. UnitName(unitGUID))
-      tracked_units[unitGUID].color = {r = 1, g = 0, b = 0, a = 1}
-      -- tracked_units[unitGUID].targeting_you = false
-    end
-  else
-    -- debug_print("not combat " .. unitGUID)
-    local c = tracked_units[unitGUID].original_bar_color
-    tracked_units[unitGUID].color = tracked_units[unitGUID].original_bar_color
-  end
-end
-
-local function GetOriginalBarColor(healthbar)
-  local r, g, b, a = healthbar:GetStatusBarColor()
-  return { r = r, g = g, b = b, a = a or 1 }
-end
-
--- Function to apply a color to a health bar
-local function SetBarColor(healthbar, color)
-  healthbar:SetStatusBarColor(color.r, color.g, color.b, color.a or 1)
-end
-
-local function UpdateTrackedUnit(unitGUID, plate)
-  local original_healthbar,original_castbar = plate:GetChildren()
-  local r,g,b,a = original_healthbar:GetStatusBarColor()
-
-  local tracked = tracked_units[unitGUID] or {}
-
-  tracked_units[unitGUID] = {
-    targeting = tracked.targeting or nil,
-    casting_at = tracked.casting_at or nil,
-    original_bar_color = tracked.original_bar_color or GetOriginalBarColor(original_healthbar),
-    cc = tracked.cc or nil,
-    healthbar = original_healthbar,
-    castbar = original_castbar,
-    color = tracked.color or { r = r, g = g, b = b, a = a },
-  }
-  local fn = plate:GetScript("OnUpdate") -- just in case someone else has modifications as well
-  plate:SetScript("OnUpdate", function (self,elapsed)
-    if fn then fn(self,elapsed) end
-    UpdatePlate(unitGUID)
-  end )
-
-  plate:SetScript("OnHide", function (self,elapsed)
-    -- reset the bar, when the game remakes the plate it might not refer to the same mob
-    local unitGUID = this:GetName(1)
-    if tracked_units[unitGUID] then
-    tracked_units[unitGUID].healthbar:SetScript("OnUpdate",nil)
-    tracked_units[unitGUID].healthbar:SetScript("OnValueChanged",nil)
-    tracked_units[unitGUID] = nil
-    end
-  end )
-
-  -- Function to maintain color on healthbar update
-  local function HealthBarUpdate()
-    local tracked = tracked_units[unitGUID]
-    if tracked then SetBarColor(this, tracked.color) end
-  end
-  tracked_units[unitGUID].healthbar:SetScript("OnUpdate",HealthBarUpdate)
-  tracked_units[unitGUID].healthbar:SetScript("OnValueChanged",HealthBarUpdate)
-end
-
-local plateTick = 0
--- local unitsTick = 0
-function Update()
-  plateTick = plateTick + arg1
-  -- unitsTick = unitsTick + arg1
-  if plateTick >= 0.25 then
-    plateTick = 0
-    local frames = { WorldFrame:GetChildren() }
-    parentcount = WorldFrame:GetNumChildren()
-    for i, plate in ipairs(frames) do
-      if plate and (plate:IsShown() and plate:IsObjectType("Button")) then
-        local unitGUID = plate:GetName(1)
-        if not tracked_units[unitGUID] then
-          UpdateTrackedUnit(unitGUID, plate)
-        end
-      end
-    end
-  -- shouldn't need this, OnHide cleans up
-  -- elseif unitsTick > 5 then -- clean units db
-  --   unitsTick = 0
-  --   for k,unit in pairs(tracked_units) do
-  --     local gone = not UnitExists(k) or UnitIsDead(k)
-  --     if gone then
-  --       debug_print("cleaning: " .. k)
-  --       tracked_units[k] = nil
-  --     end
-  --   end
-  end
-end
-
--- doesn't work
--- if ShaguTweaks and ShaguTweaks.libnameplate then
---   table.insert(ShaguTweaks.libnameplate.OnUpdate,function ()
---     UpdatePlate(this:GetName(1))
---   end,1)
---   table.insert(ShaguTweaks.libnameplate.OnInit,function()
---     local unitGUID = this:GetName(1)
---     if tonumber(unitGUID) ~= 0 and this:IsShown() and this:IsObjectType("Button") then
---       if not tracked_units[unitGUID] then
---         UpdateTrackedUnit(unitGUID, plate)
---       end
+--   -- if we can attack and we're in combat, proceed
+--   if UnitAffectingCombat("player") and (targetGUID or (UnitReaction(unitGUID,playerGUID) < 4)) then
+--     -- cc'd mob, avoid it
+--     if not targetGUID and vplate.cc == true then
+--       debug_print("cc " .. unitGUID .. " " .. UnitName(unitGUID))
+--       healthbar:SetStatusBarColor(0,0,0,0.5)
+--     -- you have aggro
+--     elseif targetGUID and (targetGUID == playerGUID) then
+--       debug_print("aggro " .. unitGUID .. " " .. UnitName(unitGUID))
+--       healthbar:SetStatusBarColor(0,1,0,1)
+--     -- targeting someone else
+--     elseif targetGUID and (targetGUID ~= playerGUID) then
+--       debug_print("aggro not-you: " .. unitGUID .. " " .. UnitName(unitGUID))
+--       healthbar:SetStatusBarColor(1,0,0,1)
+--     else
+--       -- no target
+--       debug_print("not aggro " .. unitGUID .. " " .. UnitName(unitGUID))
+--       healthbar:SetStatusBarColor(1,0,0,1)
 --     end
---   end)
+--   else
+--     local c = vplate.original_color
+--     healthbar:SetStatusBarColor(c[1],c[2],c[3],c[4])
+--   end
 -- end
 
-local function Events()
-  if event == "UNIT_CASTEVENT" then
-    -- if we see a cc get applied on a mob we have plates for, track it
-    local _,source = UnitExists(arg1)
-    local _,target = UnitExists(arg2)
-    local n,_,icon,_,_ = SpellInfo(arg4)
-    if target and tracked_units[target] and arg3 == "CAST" and icon and cc_spells[icon] then
-      tracked_units[target].cc = true
+-- local function UpdateHealth(plate,cc,original_color)
+--   -- if not plates[vplate.unit] then
+--   --   tp_print("|cffff0000impossible error:|r plate not tracked, report this")
+--   --   return
+--   -- end
+--   local unitGUID = plate:GetName(1)
+--   local _,playerGUID = UnitExists("player")
+--   local _,targetGUID = UnitExists(unitGUID.."target")
+
+--   -- if we can attack and we're in combat, proceed
+--   if UnitAffectingCombat("player") and (targetGUID or (UnitReaction(unitGUID,playerGUID) < 4)) then
+--     -- cc'd mob, avoid it
+--     if not targetGUID and cc then
+--       debug_print("cc " .. unitGUID .. " " .. UnitName(unitGUID))
+--       this:SetStatusBarColor(0,0,0,0.5)
+--     -- you have aggro
+--     elseif targetGUID and (targetGUID == playerGUID) then
+--       debug_print("aggro " .. unitGUID .. " " .. UnitName(unitGUID))
+--       this:SetStatusBarColor(0,1,0,1)
+--     -- targeting someone else
+--     elseif targetGUID and (targetGUID ~= playerGUID) then
+--       debug_print("aggro not-you: " .. unitGUID .. " " .. UnitName(unitGUID))
+--       this:SetStatusBarColor(1,0,0,1)
+--     else
+--       -- no target
+--       debug_print("not aggro " .. unitGUID .. " " .. UnitName(unitGUID))
+--       this:SetStatusBarColor(1,0,0,1)
+--     end
+--   else
+--     local c = original_color
+--     this:SetStatusBarColor(c[1],c[2],c[3],c[4])
+--   end
+-- end
+
+-- local function PlateUpdate()
+--   local guid = this:GetName(1)
+--   if not plates[guid] then
+--     tp_print("unit it not tracked")
+--     return
+--   end
+--   local healthbar = this:GetChildren()
+--   healthbar:SetScript("OnUpdate",function UpdateHealth(this,false,{ healthbar:GetStatusBarColor() }) end)
+-- end
+
+-- Copied from shagu since it resembled what I was trying to do anyway
+-- [ HookScript ]
+-- Securely post-hooks a script handler.
+-- 'f'          [frame]             the frame which needs a hook
+-- 'script'     [string]            the handler to hook
+-- 'func'       [function]          the function that should be added
+function HookScript(f, script, func)
+  local prev = f:GetScript(script)
+  f:SetScript(script, function(a1,a2,a3,a4,a5,a6,a7,a8,a9)
+    if prev then prev(a1,a2,a3,a4,a5,a6,a7,a8,a9) end
+    func(a1,a2,a3,a4,a5,a6,a7,a8,a9)
+  end)
+end
+
+local function IsNamePlate(frame)
+  return frame and (frame:IsShown() and frame:IsObjectType("Button")) and (frame:GetName(1) ~= "0x0000000000000000")
+end
+
+local function InitPlate(plate)
+  tp_print("plate init")
+
+  local guid = plate:GetName(1)
+
+  plate.guid = guid
+  _, plate.current_target = UnitExists(guid.."target")
+  plate.previous_target = nil
+  
+  plate.healthbar = plate:GetChildren()
+  plate.original_color = { plate.healthbar:GetStatusBarColor() }
+
+  plate.tick = 0
+  plate.cc = nil
+
+  HookScript(plate,"OnUpdate", function (x,y,z)
+    -- the game re-uses plates, update the mob it's for
+    plate.tick = plate.tick + arg1
+    local guid = plate:GetName(1)
+
+    local _,targeting = UnitExists(plate.guid.."target")
+    if targeting ~= plate.current_target then
+      plate.previous_target = plate.current_targete
+      plate.current_target = targeting
     end
 
-    -- mob is casting on someone, was it targeting you just before that?
-    if source and tracked_units[source] and target and arg3 == "START" then
-      tracked_units[source].casting_at = target
+    -- cc check
+    if plate.tick > 0.1 then
+      plate.tick = 0
+      plate.cc = UnitIsCC(plate.guid)
+    end
+  end)
+
+  local function UpdateHealth()
+    local _,playerGUID = UnitExists("player")
+    local plate = this:GetParent()
+    local is_hostile = UnitReaction(plate.guid,playerGUID)
+
+    -- if we can attack and we're in combat, proceed
+    if UnitAffectingCombat("player") and (plate.current_target or is_hostile) then
+      -- cc'd mob, avoid it
+      if not plate.current_target and plate.cc then
+        -- tp_print("cc " .. plate.guid .. " " .. UnitName(plate.guid))
+        this:SetStatusBarColor(0,0,0,0.5)
+      -- you have aggro
+      elseif plate.current_target and (plate.current_target == playerGUID) then
+        -- tp_print("aggro " .. plate.guid .. " " .. UnitName(plate.guid))
+        this:SetStatusBarColor(0,1,0,1)
+      -- targeting someone else
+      elseif plate.current_target and (plate.current_target ~= playerGUID) then
+        -- tp_print("aggro not-you: " .. plate.guid .. " " .. UnitName(plate.guid))
+        this:SetStatusBarColor(1,0,0,1)
+      else
+        -- no target
+        -- tp_print("not aggro " .. plate.guid .. " " .. UnitName(plate.guid))
+        this:SetStatusBarColor(1,0,0,1)
+      end
+    else
+      -- tp_print("else")
+      local c = plate.original_color
+      this:SetStatusBarColor(c[1],c[2],c[3],c[4])
     end
   end
+
+  -- might be a critter now, etc
+  plate.healthbar:SetScript("OnShow", function()
+    this:GetParent().original_color = { this:GetStatusBarColor() }
+  end)
+
+  plate.healthbar:SetScript("OnUpdate", UpdateHealth)
+  plate.healthbar:SetScript("OnValueChanged", UpdateHealth)
+end
+
+-- Copied from shagu since it resembled what I was trying to do anyway
+local initialized = 0
+local parentcount = 0
+local registry = {}
+local plateTick = 0
+local function Update()
+  plateTick = plateTick + arg1
+  if plateTick >= 0.1 then
+    plateTick = 0 
+    parentcount = WorldFrame:GetNumChildren()
+    if initialized < parentcount then
+      local children = { WorldFrame:GetChildren() }
+      for i = initialized + 1, parentcount do
+        plate = children[i]
+        if IsNamePlate(plate) and not registry[plate] then
+          InitPlate(plate)
+          registry[plate] = plate
+        end
+      end
+      initialized = parentcount
+    end
+  end
+end
+
+local function Events()
+  -- if event == "UNIT_CASTEVENT" then
+  --   -- if we see a cc get applied on a mob we have plates for, track it
+  --   local _,source = UnitExists(arg1)
+  --   local _,target = UnitExists(arg2)
+  --   local n,_,icon,_,_ = SpellInfo(arg4)
+  --   if target and plates[target] and arg3 == "CAST" and icon and cc_spells[icon] then
+  --     plates[target].cc = true
+  --   end
+
+  --   -- mob is casting on someone, was it targeting you just before that?
+  --   if source and plates[source] and target and arg3 == "START" then
+  --     plates[source].casting_at = target
+  --   end
+  -- end
 end
 
 local tankplates = CreateFrame("Frame")
--- readme.md
+++ readme.md
@@ -1,11 +1,15 @@
 # 1.12 Tank Plates
 
 * Requires [SuperWow](https://github.com/balakethelock/SuperWoW/)
-* Not currently fully compatable with ShaguTweaks nameplates, but compatable with pfUI and ShaguPlates. Turn off ShaguTweaks nameplate options to use TankPlates.  
 
 ## Features
 
 * Colors enemy plates depending on if they're attacking you
+
+## Bugs / Missing Features
+
+* Does not yet account for a mob not targeting you while casting on someone else  
+* Not fully compatable with ShaguTweaks nameplates, but compatable with pfUI and ShaguPlates. Turn off ShaguTweaks nameplate options to use TankPlates.  
 
 ___
 * Made by and for Weird Vibes of Turtle Wow  
